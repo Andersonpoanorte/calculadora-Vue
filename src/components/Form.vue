87591a73d6e10a25979535c794dab78b0a9bd7a9
Andersonpoanorte <andersonpoanorte1@gmail.com>
2024-08-26 10:44:48 -0300

    commit inicial

---
+++ src/components/Form.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps(['valorNumero1', 'valorNumero2', 'trocarFiltro']);
+</script>
+
+<template>
+    <form>
+      <div class="d-flex flex-column align-items-center mt-4 gap-3">
+        <div>
+          <input @keyup="props.valorNumero1" type="number" class="form-control">
+          <input @keyup="props.valorNumero2" type="number" class="form-control mt-3">
+        </div>
+        <div class="col">
+          <select @change="props.trocarFiltro" class="form-control text-center">
+            <option value="adicao">Adição</option>
+            <option value="multiplicacao">Multiplicação</option>
+            <option value="subtracao">Subtração</option>
+            <option value="divisao">Divisão</option>
+          </select>
+        </div>
+      </div>
+    </form>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+
+<template>
+    <header class="text-center mt-5">
+      <h1>Operações Matemáticas</h1>
+      <p>Digite os números</p>
+    </header>
+</template>
